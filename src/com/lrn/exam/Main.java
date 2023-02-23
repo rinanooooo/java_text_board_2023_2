8d3e2ce8d9ce32ecbc94ceb56aac8940e97b77d8
rinanooooo <rinanooooo@naver.com>
2023-02-23 16:05:11 +0900

    등록된 게시물 번호가 등록시마다 증가하도록

--- src/com/lrn/exam/Main.java
+++ src/com/lrn/exam/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int articleLastId = 0;
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
@@ -15,15 +16,21 @@ public class Main {
 
       if (cmd.equals("exit")) {
         break;
-      } else if (cmd.equals("/user/article/write")) {
+      }
+      else if (cmd.equals("/user/article/write")) {
         System.out.println("== 게시물 등록 ==");
         System.out.printf("제목 : ");
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String body = sc.nextLine();
-        int id = 1;
+
+        int id = articleLastId + 1;
+        // int id = ++articleLastId;
+        articleLastId++;
+        //articleLastId = id;
         System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
-      } else {
+      }
+      else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
       } // if
     } // while
