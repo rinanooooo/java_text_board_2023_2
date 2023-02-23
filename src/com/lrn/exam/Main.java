2699f9018efe2fd3e758904ae9d2ad41ddfa8a8e
rinanooooo <rinanooooo@naver.com>
2023-02-23 15:38:28 +0900

    프로그램 시작 메세지 출력

--- src/com/lrn/exam/Main.java
+++ src/com/lrn/exam/Main.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!!");
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.printf("명령) ");
+    System.out.println("== 프로그램 종료 ==");
   }
 }
