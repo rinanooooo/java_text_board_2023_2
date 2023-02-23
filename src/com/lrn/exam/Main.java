c97913534954611f6660ab44ba110164ddb1f608
rinanooooo <rinanooooo@naver.com>
2023-02-23 16:23:38 +0900

    입력받은 게시물 정보를 Article 객체에 저장 후, 객체 변수 출력

--- src/com/lrn/exam/Main.java
+++ src/com/lrn/exam/Main.java
@@ -28,7 +28,15 @@ public class Main {
         // int id = ++articleLastId;
         articleLastId++;
         //articleLastId = id;
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+
+        Article article = new Article();
+        article.id = id;
+        article.title = title;
+        article.body = body;
+
+        System.out.println("생성 된 게시물 객체 : "+article);
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
       else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
@@ -39,3 +47,9 @@ public class Main {
     sc.close();
   } // main
 } // Main
+
+class Article {
+  int id;
+  String title;
+  String body;
+}
