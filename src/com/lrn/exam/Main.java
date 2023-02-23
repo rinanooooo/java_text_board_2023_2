20a0a69b9fa0d7f78b3f4d73ad317d5cd533a637
rinanooooo <rinanooooo@naver.com>
2023-02-23 15:40:19 +0900

    exit 명령이 나올 때까지 계속 입력받기

--- src/com/lrn/exam/Main.java
+++ src/com/lrn/exam/Main.java
@@ -9,10 +9,15 @@ public class Main {
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
 
+    while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
 
+      if (cmd.equals("exit")) {
+        break;
+      }
       System.out.printf("입력된 명령어 : %s\n", cmd);
+    }
 
     System.out.println("== 프로그램 종료 ==");
 
